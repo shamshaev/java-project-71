c1489e3ab525cd3053ad9d89a792bd556a34709d
shamshaev <an.shamshaev@gmail.com>
2024-03-13 21:45:56 +0300

    Add picocli help option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,15 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public class App {
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+
         System.out.println("Hello World!");
     }
 }
